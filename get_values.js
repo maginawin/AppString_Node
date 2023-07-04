49bb4593404653c8334d74bc0f052bc82feb1e92
Maginawin <maginawin@163.com>
2023-07-04 16:21:03 +0800

    sv

--- get_values.js
+++ get_values.js
@@ -17,6 +17,9 @@
 // nfc
 // node get_values.js -f ./assert/nfc/strings_en_230616.xml ./assert/Localizable_empty.strings ./assert/nfc/values_230616.txt
 
+// Azoula Smart
+// node get_values.js -f ./assert/azoulasmart/strings_en_230704.xml ./assert/Localizable_empty.strings ./assert/azoulasmart/values_230704.txt
+
 const { program } = require('commander')
 const { writeFile, readAndroidStrings, readIOSStrings } = require('./lib/utils')
 
